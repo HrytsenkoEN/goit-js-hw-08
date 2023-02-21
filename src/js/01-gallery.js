00818c4567cbf28b1c7126451c87f6a5d1a78b81
HrytsenkoEN <ivgenius@gmail.com>
2023-02-21 23:28:45 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,56 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import { galleryItems } from './gallery-items.js';
 // Change code below this line
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-const galleryRef = document.querySelector('.gallery');
-const galleryMarkup = createGallery(galleryItems);
+const lightboxRef = document.querySelector(".gallery");
+const lightboxMarkup = createBoxItem(galleryItems);
+lightboxRef.insertAdjacentHTML(`afterbegin`, lightboxMarkup);
 
-galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
-
-function createGallery(galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `
-    <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>
-    `
-  }).join('');
+function createBoxItem(galleryItems) {
+    return galleryItems
+        .map(
+            ({ preview, original, description }) =>
+                `<a class="gallery__item" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>`
+        )
+        .join("");
 }
 
-galleryRef.addEventListener("click", onGalleryRefClick);
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+// Change code below this line
 
-let modalWindow;
-
-function onGalleryRefClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  modalWindow = basicLightbox.create(
-    `<img src='${event.target.dataset.source}' width="800" height="600">`,
-    {
-      onShow: modalWindow => {
-        window.addEventListener('keydown', onEscKeyPress);
-      },
-      onClose: modalWindow => {
-        window.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-
-  function onEscKeyPress(evt) {
-    if (evt.code === "Escape" && basicLightbox.visible()) {
-      modalWindow.close();
-    }
-  }
-  modalWindow.show();
-}
 console.log(galleryItems);
